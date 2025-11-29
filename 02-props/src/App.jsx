11ef6cba041df3d43be12a3d5ace110672331701
ShubhamPawar127 <shubham.pawar.12@outlook.com>
2025-11-29 18:52:18 +0530

    props update

--- 02-props/src/App.jsx
+++ 02-props/src/App.jsx
@@ -1,11 +1,14 @@
 import Card from "./components/Card";
+import VCard from "./components/V-Card";
 
 function App() {
   return (
-    <div className="parent">
-      <Card user="Shubham" />
+    <div className="my-2 mx-auto flex gap-4">
+      {/* <Card user="Shubham" />
       <Card user="Sham" />
-      <Card user="Danny"  />
+      <Card user="Danny"  /> */}
+      <VCard username="Shubham" btnText="Click ME"/>
+      <VCard username="Sham" btnText="Visit ME"/>
     </div>
   );
 }
