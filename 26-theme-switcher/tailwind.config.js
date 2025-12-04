9bc6ae682583c607d944c068ff5ea256a58fc643
ShubhamPawar127 <shubham.pawar.12@outlook.com>
2025-12-04 20:13:40 +0530

    theme switcher in context api

---
+++ 26-theme-switcher/tailwind.config.js
@@ -0,0 +1,9 @@
+// In tailwind.config.js
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: 'selector', // Use 'class' instead of 'selector'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
